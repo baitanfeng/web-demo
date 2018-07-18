de55a83230c6c32b0c16a88e2b7ef2f85b702097
otfngo <otfngo@163.com>
2018-07-19 00:45:57 +0800

    ...

--- api/requestAnimationFrame/requestAnimationFrame.js
+++ api/requestAnimationFrame/requestAnimationFrame.js
@@ -7,7 +7,7 @@ function step(timestamp) {
   if (!start) {
     start = timestamp
   }
-  const progress = timestamp - start
+  const progress = Math.floor(timestamp - start)
   style.left = `${Math.min(progress / 10, 200)}px`
   if (progress < 2000) {
     window.requestAnimationFrame(step)
