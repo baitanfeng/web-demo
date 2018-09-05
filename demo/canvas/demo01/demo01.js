3753ca2e16ae6e00e889ccc1b4f2a5e3eeea07bb
otfngo <otfngo@163.com>
2018-09-05 11:09:41 +0800

    ...

--- demo/canvas/demo01/demo01.js
+++
@@ -1,5 +0,0 @@
-const canvas = document.querySelector('#canvas')
-const ctx = canvas.getContext('2d')
-ctx.moveTo(0, 0)
-ctx.lineTo(200, 100)
-ctx.stroke()
--- demo/canvas/demo02/demo02.js
+++
@@ -1,5 +0,0 @@
-const canvas = document.querySelector('#canvas')
-const ctx = canvas.getContext('2d')
-ctx.beginPath()
-ctx.arc(100, 50, 40, 0, 2 * Math.PI)
-ctx.stroke()
