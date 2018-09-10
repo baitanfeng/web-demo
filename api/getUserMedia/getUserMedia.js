fafa673acb932fb571986b0aeb8fec6f8ac6cc43
otfngo <otfngo@163.com>
2018-09-10 17:53:42 +0800

    ...

--- api/getUserMedia/getUserMedia.js
+++ api/getUserMedia/getUserMedia.js
@@ -8,14 +8,12 @@ function handleButtonClick(evt) {
     video: true
   }
 
-  navigator.mediaDevices.getUserMedia(constraints)
-    .then(ms => {
-      alert(ms.toString())
-      const video = document.querySelector('video')
-      video.srcObject = ms
-      video.onloadedmetadata = () => video.play()
-    })
-    .catch(err => {
-      console.log(`name: ${err.name}, message: ${err.message}`)
-    })
+  navigator.mediaDevices.getUserMedia(constraints).then(ms => {
+    alert(ms.toString())
+    const video = document.querySelector('video')
+    video.srcObject = ms
+    video.onloadedmetadata = () => video.play()
+  }).catch(err => {
+    console.log(`name: ${err.name}, message: ${err.message}`)
+  })
 }
