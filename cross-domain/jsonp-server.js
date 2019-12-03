2aa22fb2a3a961678a4600f6c538da92be4d3a05
otfngo <otfngo@gmail.com>
2019-12-03 10:27:56 +0800

    update

--- cross-domain/jsonp-server.js
+++ cross-domain/jsonp-server.js
@@ -1,12 +1,17 @@
-let express = require("express");
-let app = express();
+const express = require("express");
+const app = express();
 
 app.use(express.static(__dirname));
-app.get("/say", function(req, res) {
-  let { wd, callback } = req.query;
-  console.log(wd);
-  console.log(callback);
-  res.end(`${callback}("我不爱你")`);
+
+app.get("/say", function (req, res) {
+    const {
+        wd,
+        callback
+    } = req.query;
+
+    console.log(wd);
+    console.log(callback);
+    res.end(`${callback}("我不爱你")`);
 });
 
 app.listen(8888);
--- cross-domain/node-proxy.js
+++ cross-domain/node-proxy.js
@@ -1,28 +1,28 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "*",
-    "Access-Control-Allow-Headers": "Content-Type"
-  });
+    res.writeHead(200, {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "Content-Type"
+    });
 
-  const proxyReq = http.request({
-    host: "127.0.0.1",
-    port: 4000,
-    url: "/",
-    method: req.method,
-    headers: req.headers
-  }, serverRes => {
-    let body = "";
-    serverRes.on("data", chunk => {
-      body += chunk;
-    });
-    serverRes.on("end", () => {
-      console.log(`the data is ${body}`);
-      res.end(body);
-    });
-  }).end();
+    const proxyReq = http.request({
+        host: "127.0.0.1",
+        port: 4000,
+        url: "/",
+        method: req.method,
+        headers: req.headers
+    }, serverRes => {
+        let body = "";
+        serverRes.on("data", chunk => {
+            body += chunk;
+        });
+        serverRes.on("end", () => {
+            console.log(`the data is ${body}`);
+            res.end(body);
+        });
+    }).end();
 });
 
 server.listen(3000);
