8aa71b46558088190275322b4516a578fca65f1c
baitanfeng <baitanfeng@163.com>
2020-11-06 20:58:13 +0800

    update

--- js/utils.js
+++ js/utils.js
@@ -49,3 +49,41 @@ export function isArrayIndex(key) {
   const numericKey = toUint32(key);
   return String(numericKey) === key && numericKey < (Math.pow(2, 32) - 1);
 }
+
+/**
+ * 转换（四舍五入/截取）数字到指定小数位数的字符串
+ * 
+ * @param {Number} value 需转换的数字
+ * @param {Number} len 小数点后数字的个数，如果忽略此参数，则默认为 0
+ * @param {Boolean} isRound 是否四舍五入，如果忽略此参数，则默认为 true
+ * 
+ * @returns {String} 给定数字的字符串
+ */
+export const toFixed = (value, len = 0, isRound = true) => {
+  const val = Number.parseFloat(value);
+
+  // 处理无效情况
+  if (Number.isNaN(val)) return value;
+  if (!Number.isInteger(len) || len < 0 || len > 100) return value;
+  if (typeof isRound !== 'boolean') return value;
+
+  // 四舍五入
+  if (isRound) {
+    return val.toFixed(len);
+  }
+
+  // 以下为截取的处理逻辑
+
+  // 处理特殊情况
+  if (len === 0) {
+    return `${Math[val >= 0 ? 'floor' : 'ceil'](val)}`;
+  }
+
+  return val.toString().replace(/(-?\d+)?(\.)?(\d+)?/, (match, p1, p2, p3) => {
+    if (p2 === undefined) {
+      return `${p1}.${'0'.repeat(len)}`;
+    } else {
+      return `${p1}${p2}${p3.substring(0, len).padEnd(len, '0')}`;
+    }
+  });
+}
---
+++ leetcode/00583_02.js
@@ -0,0 +1,36 @@
+function minDistance(word1, word2) {
+  const len1 = word1.length;
+  const len2 = word2.length;
+  const memo = new Array(len1 + 1);
+  for (let i = 0; i <= len1; i++) {
+    memo[i] = new Array(len2 + 1).fill(0);
+  }
+
+  return len1 + len2 - 2 * lcs(word1, word2, len1, len2, memo);
+}
+
+/**
+ * 最长公共子序列
+ * 
+ * @param {String} s1 待比较的字符串s1
+ * @param {String} s2 待比较的字符串s2
+ * @param {Number} m 字符串s1的前m个字符
+ * @param {Number} n 字符串s2的前n个字符
+ * @param {*} memo memo[m][n] 用来保存 lcs(s1, s2, m, n, memo) 的返回值
+ * 
+ * @returns {Number} s1与s2的最长公共子序列
+ */
+function lcs(s1, s2, m, n, memo) {
+  if (m === 0 || n === 0) return 0;
+  if (memo[m][n] > 0) return memo[m][n];
+
+  if (s1[m - 1] === s2[n - 1]) {
+    memo[m][n] = 1 + lcs(s1, s2, m - 1, n - 1, memo);
+  } else {
+    memo[m][n] = Math.max(lcs(s1, s2, m, n - 1, memo), lcs(s1, s2, m - 1, n, memo));
+  }
+
+  return memo[m][n];
+}
+
+console.log(minDistance('sea', 'ate'));
