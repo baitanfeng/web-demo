d574b83f4dbece2c4baae43947c82e2e2c34395b
baitanfeng <baitanfeng@163.com>
2020-11-19 18:26:48 +0800

    update

---
+++ vue2/src/views/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <h2>home</h2>
+    
+    <i class="el-icon-edit"></i>
+    <i class="el-icon-share"></i>
+    <i class="el-icon-delete"></i>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+
+    <van-icon name="chat-o" dot />
+    <van-icon name="chat-o" badge="9" />
+    <van-icon name="chat-o" badge="99+" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style lang="less" scoped></style>
