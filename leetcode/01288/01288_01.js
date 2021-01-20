c3207d5aca5c92de81e78aaaaa4a0bb57f64b8c7
baitanfeng <baitanfeng@163.com>
2021-01-20 10:42:56 +0800

    update leetcode 1228

--- leetcode/01288/01288_01.js
+++ leetcode/01288/01288_01.js
@@ -4,45 +4,28 @@
  * @return {Number}
  */
 function removeCoveredIntervals(intervals) {
-  const len = intervals.length;
-
-  intervals.sort((a, b) => b[0] - a[0]);
-
-  const max = new Array(len + 1);
-  for (let i = 0; i <= len; i++) {
-    max[i] = [-1, -1];
-  }
-
-  for (let i = len - 1; i >= 0; i--) {
-    const current = intervals[i];
-    const next = max[i + 1];
-
-    if (current[1] > next[1]) {
-      max[i] = [current[0], current[1]];
-    } else {
-      max[i] = [next[0], next[1]];
-    }
-  }
+  intervals.sort((a, b) => (a[0] === b[0] ? b[1] - a[1] : a[0] - b[0]));
 
   let count = 0;
-  for (let i = 0; i < len; i++) {
-    const current = intervals[i];
+  let prev = -1;
+  let curr = 0;
+  for (let i = 0, len = intervals.length; i < len; i++) {
+    curr = intervals[i][1];
 
-    if (
-      current[1] <= max[i][1] &&
-      (current[0] !== max[i][0] || current[1] !== max[i][1])
-    ) {
+    if (curr > prev) {
       count++;
+      prev = curr;
     }
   }
 
-  return len - count;
+  return count;
 }
 
 console.log(
   removeCoveredIntervals([
+    [1, 5],
     [1, 4],
-    [3, 6],
     [2, 8],
+    [3, 6],
   ])
 );
